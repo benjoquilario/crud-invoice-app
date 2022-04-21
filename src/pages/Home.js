321d79fe84cd1527abbdc81b5ed74e9362b35598
unknown <benjoquilario@gmail.com>
2022-04-21 23:32:06 +0800

    Add: pages for home or /

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import CreateInvoiceForm from '../components/Form/CreateInvoiceForm';
-
-import Header from '../components/Layout/Header';
-import Main from '../components/Layout/Main';
+import React, { useContext } from 'react';
+import InvoicesHeader from '../components/invoices/InvoicesHeader';
+import Invoices from '../components/invoices/Invoices';
+import { AppContext } from '../context/context';
 
 const Home = () => {
+  const { filterInvoices } = useContext(AppContext);
+
   return (
     <div className="min-h-screen">
-      <Header />
-      <Main />
+      <main>
+        <section className="pt-[83px] max-w-[1020px] w-full mx-auto">
+          <InvoicesHeader />
+          {filterInvoices?.length === 0 ? null : <Invoices />}
+        </section>
+      </main>
     </div>
   );
 };
