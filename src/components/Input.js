eed1dfd546c8c0f26b02b6c37d7bf5a368d67602
unknown <benjoquilario@gmail.com>
2022-04-21 23:28:48 +0800

    Add: components for input

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,13 +4,24 @@ import { useField } from 'formik';
 const Input = ({ label, ...props }) => {
   const [field, meta] = useField(props);
   return (
-    <>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <input className="text-input" {...field} {...props} />
+    <div className="flex flex-col gap-2">
+      <label
+        className="text-[12px] text-[#7388c3]"
+        htmlFor={props.id || props.name}
+      >
+        {label}
+      </label>
+      <input
+        className={`border-solid border-[1px] ${
+          meta.touched && meta.error ? 'border-[#ec5757]' : 'border-[#dfe3fa]'
+        } w-full text-[#121212] text-[11px] font-semibold rounded py-[10px] px-[18px]`}
+        {...field}
+        {...props}
+      />
       {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
+        <span className="text-[8px] text-[#ec5757]">{meta.error}</span>
       ) : null}
-    </>
+    </div>
   );
 };
 
