f6ac1878d780d6d7fd5701fca50a53175c553de6
unknown <benjoquilario@gmail.com>
2022-04-21 23:18:24 +0800

    Add: initialvalues for formik form

--- src/utilities/initialValues.js
+++ src/utilities/initialValues.js
@@ -1,19 +1,68 @@
+// Initial state formik values
+const initialValues = {
+  senderAddress: {
+    street: '',
+    city: '',
+    postCode: '',
+    country: '',
+  },
+  clientName: '',
+  clientEmail: '',
+  clientAddress: {
+    street: '',
+    city: '',
+    postCode: '',
+    country: '',
+  },
+  createdAt: new Date(),
+  paymentTerms: '30',
+  description: '',
+  items: [],
+};
+
+// initial state formik update
+const updateInitialValues = invoice => {
+  return {
+    id: invoice.id,
+    senderAddress: invoice.senderAddress,
+    clientName: invoice.clientName,
+    clientEmail: invoice.clientEmail,
+    clientAddress: invoice.clientAddress,
+    createdAt: new Date(invoice.createdAt),
+    paymentTerms: invoice.paymentTerms,
+    description: invoice.description,
+    paymentDue: invoice.paymentDue,
+    status: invoice.status,
+    items: invoice.items,
+    total: invoice.total,
+  };
+};
+
+// Option in Filter
 const optionsFilter = [
-   {
-      id: 0,
-      value: 'paid',
-      checked: false,
-   },
-   {
-      id: 1,
-      value: 'pending',
-      checked: false,
-   },
-   {
-      id: 2,
-      value: 'draft',
-      checked: false,
-   },
+  {
+    id: 0,
+    value: 'paid',
+    checked: false,
+  },
+  {
+    id: 1,
+    value: 'pending',
+    checked: false,
+  },
+  {
+    id: 2,
+    value: 'draft',
+    checked: false,
+  },
 ];
 
-export { optionsFilter };
+// PaymentTerms options
+const dropdownOptions = [
+  { name: 'Net 1 Day', value: 1 },
+  { name: 'Net 7 Days', value: 7 },
+  { name: 'Net 14 Days', value: 14 },
+  { name: 'Net 30 Days', value: 30 },
+];
+
+export { optionsFilter, dropdownOptions, initialValues, updateInitialValues };
