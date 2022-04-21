a0c2472659d69852319970d6b8f09a576886c0a1
unknown <benjoquilario@gmail.com>
2022-04-21 23:30:58 +0800

    reducers for invoices

--- src/store/reducers/invoicesReducers.js
+++ src/store/reducers/invoicesReducers.js
@@ -1,45 +1,45 @@
 export const invoicesReducers = (state, action) => {
-   switch (action.type) {
-      case 'CREATE_INVOICE':
-         return {
-            ...state.invoices,
-            invoices: [action.payload, ...state.invoices],
-         };
-      case 'UPDATE_INVOICE':
-         const updatedInvoice = action.payload;
-         const updatedInvoices = state.invoices.map(invoice => {
-            if (invoice.id === updatedInvoice.id) {
-               return updatedInvoice;
-            }
+  switch (action.type) {
+    case 'CREATE_INVOICE':
+      return {
+        ...state.invoices,
+        invoices: [action.payload, ...state.invoices],
+      };
+    case 'UPDATE_INVOICE':
+      const updatedInvoice = action.payload;
+      const updatedInvoices = state.invoices.map(invoice => {
+        if (invoice.id === updatedInvoice.id) {
+          return updatedInvoice;
+        }
 
-            return invoice;
-         });
+        return invoice;
+      });
 
-         return {
-            ...state.invoices,
-            invoices: updatedInvoices,
-         };
-      case 'DELETE_INVOICE':
-         return {
-            ...state.invoices,
-            invoices: state.invoices.filter(
-               invoice => invoice.id !== action.payload
-            ),
-         };
-      case 'MARK_AS_PAID':
-         const invoiceStatus = action.payload;
-         const updateInvoiceStatus = state.invoices.map(invoice => {
-            if (invoice.id === invoiceStatus.id) {
-               return { ...invoice, status: 'paid' };
-            }
+      return {
+        ...state.invoices,
+        invoices: updatedInvoices,
+      };
+    case 'DELETE_INVOICE':
+      return {
+        ...state.invoices,
+        invoices: state.invoices.filter(
+          invoice => invoice.id !== action.payload
+        ),
+      };
+    case 'MARK_AS_PAID':
+      const invoiceStatus = action.payload;
+      const updateInvoiceStatus = state.invoices.map(invoice => {
+        if (invoice.id === invoiceStatus.id) {
+          return { ...invoice, status: 'paid' };
+        }
 
-            return invoice;
-         });
-         return {
-            ...state.invocies,
-            invoices: updateInvoiceStatus,
-         };
-      default:
-         return state;
-   }
+        return invoice;
+      });
+      return {
+        ...state.invocies,
+        invoices: updateInvoiceStatus,
+      };
+    default:
+      return state;
+  }
 };
