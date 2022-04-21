6a4bcaa19082443cfab9ed32f74dbc11f4130c34
unknown <benjoquilario@gmail.com>
2022-04-21 23:31:39 +0800

    Add: pages for creating invoices

--- src/pages/CreateInvoice.js
+++ src/pages/CreateInvoice.js
@@ -1,1 +1,12 @@
-rafce;
+import React from 'react';
+import CreateInvoiceForm from '../components/Form/CreateInvoiceForm';
+
+const CreateInvoice = () => (
+  <section className=" pt-[83px] max-w-[1020px] w-full mx-auto">
+    <div className="bg-[#fafafa] mt-[30px]">
+      <CreateInvoiceForm />
+    </div>
+  </section>
+);
+
+export default CreateInvoice;
