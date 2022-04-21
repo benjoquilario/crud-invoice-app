ae623112099f4fbe5a5a63f89dd892e0d476c1ba
unknown <benjoquilario@gmail.com>
2022-04-21 23:17:35 +0800

    Add: function to generate a unique ID for invoice

--- src/utilities/generateId.js
+++ src/utilities/generateId.js
@@ -1,33 +1,33 @@
 const generateID = () => {
-   const idTemplate = ['X', 'X', '0', '0', '0', '0'];
-   const alphabetLength = 26;
-   const alphabetArr = Array.from(Array(alphabetLength)).map((_, index) =>
-      String.fromCharCode(index + 65)
-   );
+  const idTemplate = ['X', 'X', '0', '0', '0', '0'];
+  const alphabetLength = 26;
+  const alphabetArr = Array.from(Array(alphabetLength)).map((_, index) =>
+    String.fromCharCode(index + 65)
+  );
 
-   const uid = idTemplate.map((_, index) => {
-      if (index < 2) {
-         return alphabetArr[Math.floor(Math.random() * alphabetArr.length)];
-      } else {
-         return Math.floor(Math.random() * 10);
-      }
-   });
+  const uid = idTemplate.map((_, index) => {
+    if (index < 2) {
+      return alphabetArr[Math.floor(Math.random() * alphabetArr.length)];
+    } else {
+      return Math.floor(Math.random() * 10);
+    }
+  });
 
-   return uid.join('');
+  return uid.join('');
 };
 
 const generateUniqueId = arr => {
-   const allIdentificators = arr.map(item => {
-      return item.id;
-   });
+  const allIdentificators = arr.map(item => {
+    return item.id;
+  });
 
-   while (true) {
-      let id = generateID();
+  while (true) {
+    let id = generateID();
 
-      if (!allIdentificators.includes(id)) {
-         return id;
-      }
-   }
+    if (!allIdentificators.includes(id)) {
+      return id;
+    }
+  }
 };
 
 export default generateUniqueId;
