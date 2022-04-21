19ee4e62dddce2bb12f3c59648d5a0f6d12e6294
unknown <benjoquilario@gmail.com>
2022-04-21 23:33:25 +0800

    Add: components for Item List invoice

--- src/components/Items/Item.js
+++ src/components/Items/Item.js
@@ -12,13 +12,25 @@ const Item = ({ index, helpers }) => {
   }, [setFieldValue, index, values.items]);
 
   return (
-    <div key={index}>
-      <Input label="Item Name" name={`items[${index}].name`} />
-      <Input label="Qty." name={`items[${index}].quantity`} />
-      <Input label="Price" name={`items[${index}].price`} />
-      <Input label="Total" name={`items[${index}].total`} disabled />
-      <button type="button" onClick={() => helpers.remove(index)}>
-        Remove
+    <div key={index} className="grid md:grid-cols-[1fr_min-content] gap-2">
+      <div className="grid grid-cols-3 md:grid-cols-[2.5fr_47px_1.25fr_0.75fr] gap-2">
+        <Input label="Item Name" name={`items[${index}].name`} />
+        <Input label="Qty." name={`items[${index}].quantity`} />
+        <Input label="Price" name={`items[${index}].price`} />
+        <Input label="Total" name={`items[${index}].total`} disabled />
+      </div>
+      <button
+        className="mt-[18px]"
+        type="button"
+        onClick={() => helpers.remove(index)}
+        aria-label="delete item"
+      >
+        <svg width="13" height="16" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M11.583 3.556v10.666c0 .982-.795 1.778-1.777 1.778H2.694a1.777 1.777 0 01-1.777-1.778V3.556h10.666zM8.473 0l.888.889h3.111v1.778H.028V.889h3.11L4.029 0h4.444z"
+            fillRule="nonzero"
+          ></path>
+        </svg>
       </button>
     </div>
   );
--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -6,16 +6,19 @@ const Items = ({ name }) => {
   const { values } = useFormikContext();
 
   return (
-    <div>
-      <legend>ItemList</legend>
+    <div className="mt-[23px]">
+      <legend className="mb-5 text-[12px] text-[#452da5] font-bold">
+        ItemList
+      </legend>
       <FieldArray
         name={name}
         render={helpers => (
-          <div>
+          <div className="flex flex-col gap-4">
             {values.items.map((_, index) => (
               <Item key={index} index={index} helpers={helpers} />
             ))}
             <button
+              className="w-full font-bold text-[12px] text-white bg-[#121212] py-[13px] rounded"
               type="button"
               onClick={() =>
                 helpers.push({
