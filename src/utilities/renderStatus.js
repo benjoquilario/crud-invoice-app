6e74f396b2d9d0f09c65d4ad4733d99db658f2cf
unknown <benjoquilario@gmail.com>
2022-04-21 23:19:47 +0800

    Add: function to set background of invoice statuses

--- src/utilities/renderStatus.js
+++ src/utilities/renderStatus.js
@@ -1,4 +1,5 @@
-export const renderStatusColor = status => {
+// Function for changing backgroundColor status
+export const renderStatusBackground = status => {
   if (status === 'pending') return 'bg-[#ff8f00]';
   if (status === 'paid') return 'bg-[#74D74B]';
   if (status === 'draft') return 'bg-[#373b53]';
